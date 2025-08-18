dd1b6b73ccb68165f32c276f9ed66be83700e2f8
mugedoian@gmail.com <mugedoian@gmail.com>
2025-08-18 12:56:09 +0300

    week 2

---
+++ wk2.py
@@ -0,0 +1,12 @@
+my_list = [10, 20, 30, 40]
+my_list.extend([50, 60, 70])
+print(my_list)
+
+my_list.remove(70)
+print(my_list)
+
+my_list.sort()
+print(my_list)
+
+index_of_30 = my_list.index(30)
+print("Index of 30:", index_of_30)
